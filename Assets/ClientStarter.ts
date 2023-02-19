45c0f2aeaabab9c884986fc968bbafbdf5517f9e
seoljoo.kang <seoljoo.kang@gmail.com>
2023-02-19 19:39:13 +0900

    23.02.19-1939

---
+++ Assets/ClientStarter.ts
@@ -0,0 +1,160 @@
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import { ZepetoWorldMultiplay } from 'ZEPETO.World'
+import { Room, RoomData } from 'ZEPETO.Multiplay';
+import { Player, State, Vector3 } from 'ZEPETO.Multiplay.Schema';
+import * as UnityEngine from 'UnityEngine'
+import { CharacterState, SpawnInfo, ZepetoPlayer, ZepetoPlayers } from 'ZEPETO.Character.Controller';
+
+export default class ClientStarter extends ZepetoScriptBehaviour {
+
+    public multiplay : ZepetoWorldMultiplay;
+    private room : Room;
+    
+    private currentPlayers: Map<string, Player> = new Map<string, Player>();
+    Start() 
+    {    
+        this.multiplay.RoomCreated += (room:Room) => {
+            this.room = room;
+        }
+
+        this.multiplay.RoomJoined += (room:Room) => {
+            room.OnStateChange += this.OnStateChange;
+        }
+
+        //월드 로직 작성 2번 강의
+        this.StartCoroutine(this.SendMessageLoop(0.1));
+    }
+
+        //월드 로직 작성 2번 강의
+    private * SendMessageLoop(tick: number) 
+    {
+        while (true) 
+        {
+            yield new UnityEngine.WaitForSeconds(tick);
+
+            if (this.room != null && this.room.IsConnected) 
+            {
+                const hasPlayer = ZepetoPlayers.instance.HasPlayer(this.room.SessionId);
+                if (hasPlayer) 
+                {
+                    const myPlayer = ZepetoPlayers.instance.GetPlayer(this.room.SessionId);
+                    if (myPlayer.character.CurrentState != CharacterState.Idle)
+                    {
+                        this.SendTrasnsform(myPlayer.character.transform);
+                    }
+                }
+            }
+        }
+    }
+
+        //월드 로직 작성 2번 강의
+    private SendTrasnsform(transform: UnityEngine.Transform) 
+    {
+        const data = new RoomData();
+
+        const pos = new RoomData();
+        pos.Add("x", transform.localPosition.x);
+        pos.Add("y", transform.localPosition.y);
+        pos.Add("z", transform.localPosition.z);
+        data.Add("position", pos.GetObject());
+
+        const rot = new RoomData();
+        rot.Add("x", transform.localEulerAngles.x);
+        rot.Add("y", transform.localEulerAngles.y);
+        rot.Add("z", transform.localEulerAngles.z);
+        data.Add("rotation", rot.GetObject());
+
+        this.room.Send("onChangeTransform", data.GetObject());
+    }
+
+    private OnStateChange(state: State, isFirst: boolean)
+    {
+        if (isFirst)
+        {
+            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
+                const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
+                myPlayer.character.OnChangedState.AddListener((cur, prev) => {
+                    this.SendState(cur);
+                })
+            })
+
+        //월드 로직 작성 2번 강의
+            ZepetoPlayers.instance.OnAddedPlayer.AddListener((sessionId: string)=>{
+                const isLocal = this.room.SessionId === sessionId;
+                if (!isLocal) 
+                {
+                    const player: Player = this.currentPlayers.get(sessionId);
+
+                    player.OnChange += (ChangeValues) => this.OnUpdatePlayer(sessionId, player);
+                }
+            })
+
+        }
+
+        let join = new Map<string, Player>();
+        //월드 로직 작성 2번 강의 ++ 11:23
+        let leave = new Map<string, Player>(this.currentPlayers);
+
+        state.players.ForEach((sessionId: string, player: Player) => {
+            if (!this.currentPlayers.has(sessionId))
+            {
+                join.set(sessionId, player);
+            }
+            //월드 로직 작성 2번 강의 ++ 11:23
+            leave.delete(sessionId);
+        });
+
+        join.forEach((player: Player, sessionId: string)=> this.OnJoinPlayer(sessionId, player));
+        //월드 로직 작성 2번 강의 ++ 11:23
+        leave.forEach((player: Player, sessionId: string) => this.OnLeavePlayer(sessionId, player));
+    }
+        //월드 로직 작성 2번 강의 ++ 12:03
+    OnLeavePlayer(sessionId: string, player: Player): void {
+        console.log(`[OnRemove] players - sessionId : ${sessionId}`);
+        this.currentPlayers.delete(sessionId);
+
+        ZepetoPlayers.instance.RemovePlayer(sessionId);
+    }
+
+        //월드 로직 작성 2번 강의
+    OnUpdatePlayer(sessionId: string, player: Player) {
+        const position = this.ParseVector3(player.transform.position);
+
+        const zepetoPlayer = ZepetoPlayers.instance.GetPlayer(sessionId);
+        zepetoPlayer.character.MoveToPosition(position);
+
+        if(player.state === CharacterState.JumpIdle || player.state === CharacterState.JumpMove) 
+        {
+            zepetoPlayer.character.Jump();
+        }
+    }
+    ParseVector3(vector3: Vector3):UnityEngine.Vector3 {
+        return new UnityEngine.Vector3(
+            vector3.x,
+            vector3.y,
+            vector3.z,
+        )
+    }
+
+    private SendState(state: CharacterState) 
+    {
+        const data = new RoomData();
+        data.Add("state", state);
+        this.room.Send("onChange", data.GetObject());
+    }
+
+    private OnJoinPlayer(sessionId: string, player: Player): void {
+        console.log(`[OnJoinPlayer] players - sessionId : ${sessionId}`);
+        this.currentPlayers.set(sessionId, player);
+
+        const spawnInfo = new SpawnInfo();
+        const position = new UnityEngine.Vector3(0,0,0);
+        const rotation = new UnityEngine.Vector3(0,0,0);
+
+        spawnInfo.position = position;
+        spawnInfo.rotation = UnityEngine.Quaternion.Euler(rotation);
+
+        const isLocal = this.room.SessionId === player.sessionId;
+        ZepetoPlayers.instance.CreatePlayerWithUserId(sessionId, player.zepetoUserId, spawnInfo, isLocal);
+    }
+}
---
+++ Assets/World.multiplay/index.ts
@@ -0,0 +1,68 @@
+import { Sandbox, SandboxOptions, SandboxPlayer } from "ZEPETO.Multiplay";
+import { DataStorage } from "ZEPETO.Multiplay.DataStorage";
+import { Player, Transform, Vector3 } from "ZEPETO.Multiplay.Schema";
+
+export default class extends Sandbox {
+
+    onCreate(options: SandboxOptions) 
+    {
+        //월드 로직 작성 2번 강의
+        this.onMessage!("onChangedTransform", (client, message) => {
+            const player = this.state.players.get(client.sessionId);
+
+            const transform = new Transform();
+            transform.position = new Vector3();
+            transform.position.x = message.position.x;
+            transform.position.y = message.position.y;
+            transform.position.z = message.position.z;
+
+            transform.rotation = new Vector3();
+            transform.rotation.x = message.rotation.x;
+            transform.rotation.y = message.rotation.y;
+            transform.rotation.z = message.rotation.z;
+
+            player!.transform = transform; 
+        })
+
+        this.onMessage!("onChangedState", (client, message) => {
+            const player = this.state.players.get(client.sessionId);
+            player!.state = message.state;
+        })
+    }
+
+    async onJoin(client: SandboxPlayer) //서버측 코드
+    {
+        console.log(`[OnJoin] sessionId : ${client.sessionId}, HashCode : ${client.hashCode}, userId : ${client.userId}`)
+
+        const player = new Player();
+        player.sessionId = client.sessionId;
+
+        if (client.hashCode)
+        {
+            player.zepetoHash = client.hashCode;
+        }
+
+        if (client.userId) 
+        {
+            player.zepetoUserId = client.userId;
+        }
+
+        const storage: DataStorage = client.loadDataStorage!();
+
+        let visit_cnt = await storage.get("VisitCount") as number;
+
+        if(visit_cnt == null) visit_cnt = 0;
+
+        console.log(`[OnJoin] ${client.sessionId}'s visiting count : ${visit_cnt}`);
+
+        await storage.set("VisitCount", ++visit_cnt);
+
+        this.state.players.set(client.sessionId, player);
+    }
+
+    onLeave(client: SandboxPlayer, consented?: boolean) 
+    {
+        //월드 로직 작성 2번 강의
+        this.state.players.delete(client.sessionId);
+    }
+}
