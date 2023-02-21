4eb2e6a0db2939cb25263d8f59a6c2a64a29d6ef
seoljoo.kang <seoljoo.kang@gmail.com>
2023-02-21 09:34:18 +0900

    23.02.21-0934
    디자인만 남음

--- Assets/Scripts/Client.ts
+++ Assets/Scripts/Client.ts
@@ -2,7 +2,7 @@ import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 import { ZepetoWorldMultiplay } from 'ZEPETO.World'
 import { Room, RoomData } from 'ZEPETO.Multiplay';
 import { Player, State, Vector3 } from 'ZEPETO.Multiplay.Schema';
-import { CharacterState, SpawnInfo, ZepetoPlayer, ZepetoPlayers } from 'ZEPETO.Character.Controller';
+import { CharacterState, SpawnInfo, ZepetoPlayers } from 'ZEPETO.Character.Controller';
 import * as UnityEngine from 'UnityEngine'
 
 export default class Client extends ZepetoScriptBehaviour {
@@ -24,33 +24,12 @@ export default class Client extends ZepetoScriptBehaviour {
             room_from_server.OnStateChange += this.OnStateChange;
         };
 
-        //클라이언트 측에서 서버로 0.1초마다 소식을 보내는 코루틴 함수 실행
+        //클라이언트 측에서 서버로 0.1초마다 자신의 정보를 전송하는 코루틴 함수 실행
         this.StartCoroutine(this.SendMessageLoop(0.1));
     }
 
     private OnStateChange(state: State, isFirst: boolean)
     {
-        if (isFirst)
-        {
-            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
-                const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
-                myPlayer.character.OnChangedState.AddListener((cur, prev) => {
-                    this.SendState(cur);
-                })
-            })
-
-            ZepetoPlayers.instance.OnAddedPlayer.AddListener((sessionId: string)=>{
-                const isLocal = this.room.SessionId === sessionId;
-                if (!isLocal) 
-                {
-                    const player: Player = this.currentPlayers.get(sessionId);
-
-                    player.OnChange += () => this.OnUpdatePlayer(sessionId, player); 
-                }
-            })
-
-        }
-
         let join = new Map<string, Player>();
         let leave = new Map<string, Player>(this.currentPlayers);
 
@@ -61,39 +40,34 @@ export default class Client extends ZepetoScriptBehaviour {
             }
             leave.delete(sessionId);
         });
-
         join.forEach((player: Player, sessionId: string)=> this.OnJoinPlayer(sessionId, player));
         leave.forEach((player: Player, sessionId: string) => this.OnLeavePlayer(sessionId, player));
-    }
 
-    private SendState(state: CharacterState) 
-    {
-        const data = new RoomData();
-        data.Add("state", state);
-        this.room.Send("onChangedState", data.GetObject());
-    }
-
-    private OnUpdatePlayer(sessionId: string, player: Player) {
-        const position = this.ParseVector3(player.transform.position);
-
-        const zepetoPlayer = ZepetoPlayers.instance.GetPlayer(sessionId);
-        zepetoPlayer.character.MoveToPosition(position);
-
-        if(player.state === CharacterState.JumpIdle || player.state === CharacterState.JumpMove) 
+        if (isFirst)
         {
-            zepetoPlayer.character.Jump();
+            //본 클라이언트의 아바타 상태(정지, 이동 등)를 서버로 전송하는 이벤트 리스너
+            //추후 코루틴으로 전달할 transform과 달리 사건 발생시에만 전달
+            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
+                const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
+                myPlayer.character.OnChangedState.AddListener((cur, prev) => {
+                    this.SendState(cur);
+                })
+            })
+
+            //각 클라이언트 별 아바타 위치를 서버로부터 전달받아 본 클라이언트에서 추적하는 이벤트 리스너
+            ZepetoPlayers.instance.OnAddedPlayer.AddListener((sessionId: string)=>{
+                const isLocal = this.room.SessionId === sessionId;
+                if (!isLocal) 
+                {
+                    const player: Player = this.currentPlayers.get(sessionId);
+
+                    player.OnChange += () => this.OnUpdatePlayer(sessionId, player); 
+                }
+            })
         }
-    }
-    private ParseVector3(vector3: Vector3):UnityEngine.Vector3 {
-        return new UnityEngine.Vector3(
-            vector3.x,
-            vector3.y,
-            vector3.z,
-        )
     }
 
     private OnJoinPlayer(sessionId: string, player: Player): void {
-        console.log(`[OnJoinPlayer] players - sessionId : ${sessionId}`);
         this.currentPlayers.set(sessionId, player);
 
         const spawnInfo = new SpawnInfo();
@@ -108,13 +82,33 @@ export default class Client extends ZepetoScriptBehaviour {
     }
 
     private OnLeavePlayer(sessionId: string, player: Player): void {
-        console.log(`[OnRemove] players - sessionId : ${sessionId}`);
         this.currentPlayers.delete(sessionId);
 
         ZepetoPlayers.instance.RemovePlayer(sessionId);
     }
 
-    //클라이언트 측에서 서버로 0.1초마다 소식을 보내는 코루틴 함수 정의
+    private SendState(state: CharacterState) //state는 정지, 걷기 등의 아바타가 지닌 다양한 상태
+    {
+        const data = new RoomData();
+        data.Add("state", state);
+        this.room.Send("onChangedState", data.GetObject());
+    }
+
+    private OnUpdatePlayer(sessionId: string, player: Player) {
+        const position = this.ParseVector3(player.transform.position);
+
+        const zepetoPlayer = ZepetoPlayers.instance.GetPlayer(sessionId);
+        zepetoPlayer.character.MoveToPosition(position);
+    }
+    private ParseVector3(vector3: Vector3):UnityEngine.Vector3 {
+        return new UnityEngine.Vector3(
+            vector3.x,
+            vector3.y,
+            vector3.z,
+        )
+    }
+
+    //본 클라이언트 측에서 서버로 0.1초마다 본 클라이언트 아바타의 transform 정보를 보내는 코루틴 함수 정의
     private * SendMessageLoop(tick: number) 
     {
         while (true) 
@@ -127,10 +121,14 @@ export default class Client extends ZepetoScriptBehaviour {
                 const hasPlayer = ZepetoPlayers.instance.HasPlayer(this.room.SessionId); 
                 if (hasPlayer) //sessionId가 있으면
                 {
-                    const myPlayer = ZepetoPlayers.instance.GetPlayer(this.room.SessionId); //myPlayer에 클라이언트의 정보를 담고
-                    if (myPlayer.character.CurrentState != CharacterState.Idle) //myPlayer, 즉 클라이언트가 정지 중이 아니라면(움직이면),
+                    //myPlayer에 클라이언트의 정보를 담고
+                    const myPlayer = ZepetoPlayers.instance.GetPlayer(this.room.SessionId); 
+
+                    //myPlayer, 즉 클라이언트가 정지 중이 아니라면(움직이면),
+                    if (myPlayer.character.CurrentState != CharacterState.Idle) 
                     {
-                        this.SendTransform(myPlayer.character.transform); //클라이언트의 transform값을 넣어 SendTransform 함수 실행
+                        //클라이언트의 transform값을 넣어 SendTransform 함수 실행
+                        this.SendTransform(myPlayer.character.transform); 
                     }
                 }
             }
@@ -154,6 +152,8 @@ export default class Client extends ZepetoScriptBehaviour {
         rot.Add("z", transform.localEulerAngles.z);
         data.Add("rotation", rot.GetObject()); //data변수에 rotation이란 name으로 rot을 객체화 해서 추가
 
-        this.room.Send("onChangedTransform", data.GetObject()); //서버의 Room에 onChangedTransform 이벤트로 data를 객체화하여 전달
+        //서버의 Room에 onChangedTransform 이벤트로 data를 객체화하여 전달
+        this.room.Send("onChangedTransform", data.GetObject()); 
     }
 }
+
